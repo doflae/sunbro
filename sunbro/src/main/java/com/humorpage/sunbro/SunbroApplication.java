fb63d5d0bd8d74f839b109039e9e25788c662fb8
seono <tjsh0111@gmail.com>
2020-12-01 14:29:00 +0900

    init

---
+++ sunbro/src/main/java/com/humorpage/sunbro/SunbroApplication.java
@@ -0,0 +1,13 @@
+package com.humorpage.sunbro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunbroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunbroApplication.class, args);
+	}
+
+}
---
+++ sunbro/src/test/java/com/humorpage/sunbro/SunbroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humorpage.sunbro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunbroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
